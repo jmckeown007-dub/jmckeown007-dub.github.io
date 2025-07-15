b9e51b98a9f9b171bfc9f5ebd511a8b7883149e8
jmckeown007-dub <jaydubtwelve@gmail.com>
2025-07-14 23:22:39 -0500

    replacing old files for new icons

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function openModal() {
 function closeModal() {
   document.getElementById("loveModal").style.display = "none";
 }
+
+const heart = document.getElementById('heartCursor');
+
+window.addEventListener('mousemove', e => {
+  // Offset by half cursor size to center it on pointer
+  heart.style.left = (e.clientX - 16) + 'px';
+  heart.style.top = (e.clientY - 16) + 'px';
+
+  // For getting element under mouse
+  const hoveredElem = document.elementFromPoint(e.clientX, e.clientY);
+
+
+  // add glow if hovered element is button or link
+  if (hoveredElem && (hoveredElem.tagName === 'BUTTON' || 
+    hoveredElem.tagName === 'A' ||
+    hoveredElem.classList.contains('close'))
+    ) 
+    {
+    heart.classList.add('fish');
+  } else {
+    heart.classList.remove('fish');
+  }
+});
+
