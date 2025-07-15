8f2ddbae95a32994a930ea1e25806d848e1e361a
jmckeown007-dub <jaydubtwelve@gmail.com>
2025-07-14 21:35:08 -0500

    new popups

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function showMessage() {
   alert("I love you more than the beauty of the colors of my hair and your eyes combined 💕");
 }
+function openModal() {
+  document.getElementById("loveModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("loveModal").style.display = "none";
+}
